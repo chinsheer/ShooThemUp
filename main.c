9081c36b00eb53a330b4f46613712b11de8991bb
chinsheer <chinsheer05043@gmail.com>
2022-10-19 00:01:48 +0900

    refactor code

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 
 Vector2 myAmmoShape[9] = {{0, -1}};
 
+
 typedef struct ammo
 {
     int alive;
@@ -17,12 +18,6 @@ typedef struct ammo
     int size;
 } ammo;
 
-typedef struct monster
-{
-    Vector2 position;
-    Vector2 shape[81];
-} monster;
-
 typedef struct object_buffer
 {
     int last;
@@ -32,11 +27,15 @@ typedef struct object_buffer
 typedef struct plane
 {
     Vector2 position;
-    Vector2 shape[25];
-    int shootCD;
+    Vector2 shape[81];
     object_buffer ammo_buffer;
+    int size;
+    int shootCD;
+    int CD;
 
 } plane;
+
+// ammo-------------------------------------------------------------------------
 
 ammo shoot(Vector2 pos, int speed, int dmg, float angle, int size){
     ammo temp;
@@ -100,6 +99,41 @@ void add_ammo(plane *p, ammo bullet){
         buffer++;
     }
 }
+// ammo-------------------------------------------------------------------------
+
+// plane------------------------------------------------------------------------
+plane plane_constructor(Vector2 *shape, int shape_size, int cd, Vector2 position, int size){
+    plane temp;
+    temp.ammo_buffer.last = 0;
+    temp.position = position;
+    temp.size = size;
+    temp.shootCD = cd;
+    temp.CD = 0;
+    memset(temp.shape, 0, sizeof(temp.shape));
+    memset(temp.ammo_buffer.buffer, 0, sizeof(temp.ammo_buffer.buffer));
+    memmove(temp.shape, shape, shape_size);
+    return temp;
+}
+
+void plane_draw(plane p){
+    Vector2 *temp = p.shape;
+    DrawRectangleV(p.position, Vector2Scale(Vector2One(), p.size), RED);
+    while (!Vector2Equals(*temp, Vector2Zero())){
+        DrawRectangleV(Vector2Add(p.position, Vector2Scale(*temp, p.size)), Vector2Scale(Vector2One(), p.size), BLACK);
+        temp++;
+    }
+}
+
+int cooldown(plane *p){
+    if(p->CD == 0){
+        p->CD = p->shootCD;
+        return 1;
+    }
+    p->CD--;
+    return 0;
+}
+
+// plane------------------------------------------------------------------------
 
 int main(void){
     //core
@@ -112,25 +146,31 @@ int main(void){
     Vector2 playerIntPosition = {800, 450};
     const int playerSize = 10;
     const int speed = 3;
+
+    //monster
+    Vector2  monsterIntPosition = {800, 100};
+    const int monsterSize = 10;
+
+    //draw monster
+    //set monster shape and properties
+    Vector2 myMonster[] = {{4, -4}, {3, -4}, {2, -4}, {1, -4}, {0, -4}, {-1, -4}, {-2, -4}, {-3, -4}, {-4, -4}, 
+    {4, -3}, {0, -3}, {-4, -3}, 
+    {4, -2}, {0, -2}, {-4, -2}, 
+    {4, -1}, {0, -1}, {-4, -1}, {-1, -1}, {1, -1}, 
+    {4, 0}, {3, 0}, {2, 0}, {1, 0}, {-1, 0}, {-2, 0}, {-3, 0}, {-4, 0},
+    {4, 1}, {3, 1}, {2, 1}, {1, 1}, {-1, 1}, {-2, 1}, {-3, 1}, {-4, 1},
+    {4, 2}, {3, 2}, {2, 2}, {-2, 2}, {-3, 2}, {-4, 2},
+    {4, 3}, {3, 3}, {2, 3}, {-2, 3}, {-3, 3}, {-4, 3},
+    {4, 4}, {3, 4}, {2, 4}, {1, 4}, {0, 4}, {-1, 4}, {-2, 4}, {-3, 4}, {-4, 4}};
+    plane test_monster = plane_constructor(myMonster, sizeof(myMonster), 10, monsterIntPosition, 10);
         
     //draw player
     //set player shape and properties
-    plane player;
-    player.shootCD = 0;
-    memset(&(player.shape), 0, sizeof(player.shape)); //clean memory
-    Vector2 myPlane[25] = {{0, -1}, {1, 0}, {-1, 0}, {0, 1}, {-1, 1}, {1, 1}, {-2, 2}, {-1, 2}, {0, 2}, {1, 2}, {2, 2}}; //compound literals saves our day
-    memmove(&(player.shape), myPlane, sizeof(player.shape));
-    player.position = playerIntPosition;
-
-    //ammo section
-    player.ammo_buffer.last = 0;
-    memset(player.ammo_buffer.buffer, 0, sizeof(player.ammo_buffer.buffer)); //clean ammo_buffer_memory
+    Vector2 myPlane[] = {{0, -1}, {1, 0}, {-1, 0}, {0, 1}, {-1, 1}, {1, 1}, {-2, 2}, {-1, 2}, {0, 2}, {1, 2}, {2, 2}}; //compound literals saves our day
+    plane player = plane_constructor(myPlane, sizeof(myPlane), 10, playerIntPosition, 10);
 
     //main windows
     while (!WindowShouldClose()){
-
-        //gameplay section
-        if(player.shootCD > 0) player.shootCD--;
 
         //control section
         if(IsKeyDown(KEY_RIGHT)) player.position.x += speed;
@@ -138,13 +178,10 @@ int main(void){
         if(IsKeyDown(KEY_UP)) player.position.y -= speed;
         if(IsKeyDown(KEY_DOWN)) player.position.y += speed;
         if(IsKeyDown(KEY_Z)) {
-            if(player.shootCD == 0){
-                player.shootCD = 10;
-                add_ammo(&player, shoot(player.position, 3, 10, -135.0f, 10));
-            }
+            if(cooldown(&player)) add_ammo(&player, shoot(player.position, 3, 10, -135.0f, 10));
         }
         if(IsKeyDown(KEY_X)){
-            int a = 0;
+            if(cooldown(&test_monster)) add_ammo(&test_monster, shoot(test_monster.position, 5, 10, 45.0f, 10));
         }
 
         BeginDrawing();
@@ -152,14 +189,22 @@ int main(void){
             ClearBackground(RAYWHITE);
             // player draw section
             Vector2 *playerDrawPixel = player.shape;
-            DrawRectangleV(player.position, Vector2Scale(Vector2One(), playerSize), BLACK);
+            DrawRectangleV(player.position, Vector2Scale(Vector2One(), playerSize), RED);
             while (!Vector2Equals(*playerDrawPixel, Vector2Zero())){
                 DrawRectangleV(Vector2Add(player.position, Vector2Scale(*playerDrawPixel, playerSize)), Vector2Scale(Vector2One(), playerSize), BLACK);
                 playerDrawPixel++;
             }
 
+            Vector2 *monsterDrawPixel = test_monster.shape;
+            DrawRectangleV(test_monster.position, Vector2Scale(Vector2One(), monsterSize), RED);
+            while (!Vector2Equals(*monsterDrawPixel, Vector2Zero())){
+                DrawRectangleV(Vector2Add(test_monster.position, Vector2Scale(*monsterDrawPixel, playerSize)), Vector2Scale(Vector2One(), monsterSize), BLACK);
+                monsterDrawPixel++;
+            }
+
             // ammo draw section
             draw_ammo(&player);          
+            draw_ammo(&test_monster);
 
         EndDrawing();
     }
